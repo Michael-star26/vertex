aa50eb5be5dceaf0a970f0b651faa87cb6b3ab9d
Michael-star26 <130248327+Michael-star26@users.noreply.github.com>
2024-09-21 03:18:26 +0300

    t commit

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
-import { Component , ChangeDetectionStrategy} from '@angular/core';
-import { TuiAppearance, TuiButton, TuiSurface, TuiTitle } from '@taiga-ui/core';
+import { Component , ChangeDetectionStrategy, inject} from '@angular/core';
+import { TuiAlertService, TuiAppearance, TuiButton, TuiSurface, TuiTitle } from '@taiga-ui/core';
 import { TuiCardLarge, TuiCardMedium, TuiHeader } from '@taiga-ui/layout';
 import {TuiRepeatTimes} from '@taiga-ui/cdk'
 import {TuiBadge, TuiFade} from '@taiga-ui/kit'
@@ -19,13 +19,15 @@ import {TuiBadge, TuiFade} from '@taiga-ui/kit'
     TuiRepeatTimes,
     TuiFade,
     TuiAppearance,
-    TuiButton
+    TuiButton,
+    
   ],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.less',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ProjectsComponent {
+  private readonly alerts=inject(TuiAlertService)
   project:any=[
     {
       name:"outube App",
@@ -70,5 +72,26 @@ export class ProjectsComponent {
       desc:"desc"
     }
   ]
+
+  eCommerce():void{
+    this.alerts.open('This project is still under development. It can only be previewed on a computer',{label:'Warning',appearance:'warning'}).subscribe();
+  }
+
+  vertex():void{
+    this.alerts.open('This is my tech startup company',{label:'success',appearance:'Notification'}).subscribe();
+  }
+
+  todo():void{
+    this.alerts.open('This is a simple todo app. Note that any inputs will be stored dynamically. Therefore if you refresh the website, the data will be lost',{label:'warning',appearance:'Notification'}).subscribe();
+  }
+
+  utube():void{
+    this.alerts.open('This is a simple youtube search app. The maximum search results are set to 50. If you exeed the quota limit provided by YouTube, you may get a 500 error message',{label:'warning',appearance:'Notification'}).subscribe();
+  }
+
+  social():void{
+    this.alerts.open('This is a simple reddit-like app. This is because it uses the concept of upvoting and downvoting.The posts are then sorted according to the number of votes',{label:'success',appearance:'Notification'}).subscribe();
+  }
+
   }
 
